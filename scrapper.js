6ed288f30f6e065c1ee8f841ca4d98fcd39f7a2f
marior9 <mariorodriguezan@gmail.com>
2019-12-15 15:41:07 -0500

    multiple page scraping process

--- scrapper.js
+++ scrapper.js
@@ -15,51 +15,56 @@ let scrape = (async () => {
   const page = await browser.newPage();
   await page.goto(site, {waitUntil: 'load', timeout: 0});
 
-  const text = await page.evaluate(() => { 
-    document.getElementsByClassName("section-result"); 
-  });
-  debugger;
-  console.log(text)
-
-
- 
 
   await page.evaluate(() => { 
     document.getElementsByClassName("section-result")[0].click(); 
   });
+
   await page.screenshot({path: 'example.png'});
   await page.waitFor(4000);
+  
   const result = await page.evaluate(() => {
     let check = document.getElementsByClassName("section-info-text")[4].innerText
     let closeRest = ""
     console.log(check)
-    // debugger;
+
+    let busyStateNotLive = null
     if (check == "Closed." || check =="Cerrado."){
-    closeRest = "RESTAURANTs ARE CLOSE! IN THIS AREA"
-    console.log(closeRest)
-    debugger;
-    return {
-      closeRest
-    }
-    // debugger
-    }else{
+      closeRest = "RESTAURANTs ARE CLOSE! IN THIS AREA"
+      console.log(closeRest)
       
-    let busyState = document.getElementsByClassName("section-popular-times-live-description")[0].innerText
-    let percent = document.getElementsByClassName("section-popular-times-value section-popular-times-live-value")[0].attributes[0].textContent
-    let restName = document.getElementsByClassName('GLOBAL__gm2-headline-5 section-hero-header-title-title')[0].innerText
-    return {
-      busyState,
-      percent,
-      restName
-    }  
-  }
+      return {
+        closeRest
+      }
+
+
+      }else if (busyStateNotLive == ""){
+          busyStateNotLive = document.getElementsByClassName("section-popular-times-value section-popular-times-current-value")[0].parentElement.attributes[0].textContent.slice(0,3) 
+          let restName = document.getElementsByClassName('GLOBAL__gm2-headline-5 section-hero-header-title-title')[0].innerText
+  
+          return {
+            busyStateNotLive,
+            restName 
+          }
+        
+      }else{
+      let percent = document.getElementsByClassName("section-popular-times-value section-popular-times-live-value")[0].attributes[0].textContent
+      let restName = document.getElementsByClassName('GLOBAL__gm2-headline-5 section-hero-header-title-title')[0].innerText
+       
+      return {
+        percent,
+        restName
+      }  
+    }
 
   });    
+
     console.log(result)
-    await page.waitFor(400000);
+      await page.evaluate(() => { 
+        document.getElementsByClassName("section-back-to-list-button blue-link noprint")[0].click(); 
+      }); 
     browser.close();
     return result;
-
-
 })();
+
 
