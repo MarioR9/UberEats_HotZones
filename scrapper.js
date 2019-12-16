ad31bc9b112d4bc91b083d37572fef015d16c9ec
marior9 <mariorodriguezan@gmail.com>
2019-12-15 23:46:38 -0500

    scrape betweeb pages programatically

--- scrapper.js
+++ scrapper.js
@@ -2,23 +2,25 @@ const puppeteer = require('puppeteer');
 
 let site = "https://www.google.com/maps/search/restaurants+near+springfield+va/@38.7836362,-77.1906504,15z/data=!3m1!4b1"
 
-// var i;
-// for (i = 0; i < cars.length; i++) {
-//   text += cars[i] + "<br>";
-// }
-
 
 let scrape = (async () => {
-  
+let array = []
+
   const browser = await puppeteer.launch(
-      {headless: false});
+      {headless: false},
+      {ignoreDefaultArgs: ['--disable-extensions'],}
+      );
   const page = await browser.newPage();
   await page.goto(site, {waitUntil: 'load', timeout: 0});
+  await page.waitFor(4000);
 
-
-  await page.evaluate(() => { 
-    document.getElementsByClassName("section-result")[0].click(); 
-  });
+for (let i = 0; i < 
+  5; i++) {
+    console.log("first:"+ i)
+  await page.evaluate((i) => { 
+    document.getElementsByClassName("section-result")[i].click(); 
+    
+  },i);
 
   await page.screenshot({path: 'example.png'});
   await page.waitFor(4000);
@@ -28,17 +30,9 @@ let scrape = (async () => {
     let closeRest = ""
     console.log(check)
 
-    let busyStateNotLive = null
-    if (check == "Closed." || check =="Cerrado."){
-      closeRest = "RESTAURANTs ARE CLOSE! IN THIS AREA"
-      console.log(closeRest)
-      
-      return {
-        closeRest
-      }
+    let busyStateNotLive = ""
 
-
-      }else if (busyStateNotLive == ""){
+    if (busyStateNotLive != null){
           busyStateNotLive = document.getElementsByClassName("section-popular-times-value section-popular-times-current-value")[0].parentElement.attributes[0].textContent.slice(0,3) 
           let restName = document.getElementsByClassName('GLOBAL__gm2-headline-5 section-hero-header-title-title')[0].innerText
   
@@ -58,16 +52,19 @@ let scrape = (async () => {
     }
 
   });    
-
+    array.push(result)
     console.log(result)
       await page.evaluate(() => { 
         document.getElementsByClassName("section-back-to-list-button blue-link noprint")[0].click(); 
       }); 
+      await page.waitFor(4000);
+      
+  }
+  console.log(array)
+  debugger
     browser.close();
     return result;
 })();
-
-
 
 // await page.evaluate(() => { 
 //   document.getElementsByClassName("section-back-to-list-button blue-link noprint")[0].click(); 
