e56f2cd0db57708d4c952099d549a356256aad78
marior9 <mariorodriguezan@gmail.com>
2019-12-19 16:08:16 -0500

    renamed variables

--- scrapper.js
+++ scrapper.js
@@ -4,73 +4,109 @@ let site = "https://www.google.com/maps/search/restaurants+near+springfield+va/@
 
 
 let scrape = (async () => {
-let array = []
 
+let sectionsArray = []
+let pageCounter = 1
+let sectionResultCollection = []
   const browser = await puppeteer.launch(
       {headless: false},
       {ignoreDefaultArgs: ['--disable-extensions'],}
       );
   const page = await browser.newPage();
   await page.goto(site, {waitUntil: 'load', timeout: 0});
-  await page.waitFor(4000);
+  await page.waitFor(2000);
 
+  let section = await page.evaluate(() => { 
+    let Divs = document.getElementsByClassName("section-result").length;
+
+    return {
+        Divs
+    }
+  },);
+
+  sectionResultCollection.push(section)
+
+  if(sectionResultCollection[0].Divs == 21){
+    console.log("Ads present")
+  }else{
+    console.table(sectionResultCollection)
+  }
+  await page.waitFor(2000);
 
 for (let i = 0; i < 
-  5; i++) {
-    console.log("first:"+ i)
+  sectionResultCollection[0].Divs + 1; i++) {
   await page.evaluate((i) => { 
     document.getElementsByClassName("section-result")[i].click(); 
-    
   },i);
 
   await page.screenshot({path: 'example.png'});
-  await page.waitFor(4000);
+
+  await page.waitForSelector('.section-info-text', {
+    visible: true,
+  });
   
   const result = await page.evaluate(() => {
     let check = document.getElementsByClassName("section-info-text")[4].innerText
     let check2 = document.getElementsByClassName("section-info-text")[3].innerText
-    console.log(check)
-    console.log(check2)
-    let busyStateNotLive = ""
+    let check3 = document.getElementsByClassName('GLOBAL__gm2-subtitle-alt-1')[0].innerText
+    let check4 = document.getElementsByClassName("section-popular-times-value section-popular-times-live-value").length
+    let check5 = document.getElementsByClassName("section-popular-times-value section-popular-times-current-value")
 
-    if(check == "Cerrado." || check2 == "Cerrado." ){
-      busyStateNotLive = "0%"
-      let restName = document.getElementsByClassName('GLOBAL__gm2-headline-5 section-hero-header-title-title')[0].innerText
+    let bussynessPercentage = ""
+    
+    if((check == "Cerrado." || check2 == "Cerrado.") || (check3 != "Horarios populares" && check5.length == 0)){
+      bussynessPercentage = "0%"
+      let restaurantName = document.getElementsByClassName('GLOBAL__gm2-headline-5 section-hero-header-title-title')[0].innerText
 
       return {
-        busyStateNotLive,
-        restName 
+        bussynessPercentage,
+        restaurantName 
       }
-
-    }else if (busyStateNotLive != null){
-          busyStateNotLive = document.getElementsByClassName("section-popular-times-value section-popular-times-current-value")[0].parentElement.attributes[0].textContent.slice(0,3) 
-          let restName = document.getElementsByClassName('GLOBAL__gm2-headline-5 section-hero-header-title-title')[0].innerText
+    }else if (check4 == 0){
+          bussynessPercentage = document.getElementsByClassName("section-popular-times-value section-popular-times-current-value")[1].parentElement.attributes[0].textContent.slice(0,3) 
+          let restaurantName = document.getElementsByClassName('GLOBAL__gm2-headline-5 section-hero-header-title-title')[0].innerText
   
           return {
-            busyStateNotLive,
-            restName 
+            bussynessPercentage,
+            restaurantName 
           }
         
-      }else{
-      let percent = document.getElementsByClassName("section-popular-times-value section-popular-times-live-value")[0].attributes[0].textContent
-      let restName = document.getElementsByClassName('GLOBAL__gm2-headline-5 section-hero-header-title-title')[0].innerText
+      }else if(check4 == 1){
+        bussynessPercentage = document.getElementsByClassName("section-popular-times-value section-popular-times-live-value")[0].attributes[0].textContent
+      let restaurantName = document.getElementsByClassName('GLOBAL__gm2-headline-5 section-hero-header-title-title')[0].innerText
        
       return {
-        percent,
-        restName
+        bussynessPercentage,
+        restaurantName
       }  
+
     }
 
-  });    
-    array.push(result)
-    console.log(result)
+  });
+    sectionsArray.push(result)
+    
+    
       await page.evaluate(() => { 
         document.getElementsByClassName("section-back-to-list-button blue-link noprint")[0].click(); 
       }); 
-      await page.waitFor(4000);
-      
+      await page.waitFor(2000);
+    if(i == sectionResultCollection[0].Divs - 1){
+      i = 0
+      await page.evaluate(() => { 
+        document.getElementsByClassName("n7lv7yjyC35__button-next-icon")[0].click(); 
+      });
+      await page.waitFor(2000); 
+      console.log("page: " + pageCounter)
+      pageCounter++
+
+      if(pageCounter == 4){
+        console.log(sectionsArray)
+        browser.close();
+        return sectionsArray;
+      }
+    }
+    
   }
-  console.log(array)
-    browser.close();
-    return array;
+  
 })();
+
