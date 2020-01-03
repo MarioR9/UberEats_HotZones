93b1dd153a448ebb2a223195114cedd8a263b790
marior9 <mariorodriguezan@gmail.com>
2020-01-03 11:35:17 -0500

    map radius working with coords

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import MapView from 'react-native-maps';
+import MapView , { AnimatedRegion }  from'react-native-maps';
+
 import {
   Image, 
   TouchableOpacity, 
   TextInput, 
   View, 
-  Alert}
+  Alert
+       }
 from 'react-native';
 
 
@@ -15,11 +17,13 @@ export default function App() {
   const [hidden, setHidden] = React.useState(true);
   const [lat, setLat] = React.useState(0)
   const [lon, setLon] = React.useState(0)
-
+  const [busyColor, setBusyColor] = React.useState('#fc000046')
+  const [idleColor, setIdleColor] = React.useState('#02bd0c2a')
+  const [semyBusyColor, setSemiBusyColor]= React.useState('#e7d42d4f')
   const localHost = ""
-
   
-  startFetch=()=>{
+  
+  handleFetch=()=>{
     console.log("fetch sent")
     console.log("this is the value: " + value)
     const data = { location: value}
@@ -46,45 +50,28 @@ export default function App() {
       )
       setLat(parseFloat(data.lat))
       setLon(parseFloat(data.lon))
+      
       console.log("received.")
     }).catch(err => {
       // Error handling
       console.log("Error Reading data " + err);
     });
   }
-
-
+  
   return (
  
     <View style={{flex: 1}}>
     {
-    hidden == true
-    ?
-    <TouchableOpacity onPress={()=>setHidden(false)}>
-        <Image style={{width: 130, height: 130}} source={require('./assets/search.png')} />
-    </TouchableOpacity>
-     :
-    <TextInput
-    style={{ marginTop: 20, width: 120, height: 30, right: -20,borderColor: 'red', borderWidth: 1}}
-    onChangeText={text => onChangeText(text)}
-    placeholder="  Springfield,VA"
-    placeholderTextColor="gray"
-    value={value}
-   />
-    }
-     <TouchableOpacity onPress={()=>this.startFetch()}>
-        <Image style={{width: 130, height: 130}} source={require('./assets/SearchLogo.png')} />
-    </TouchableOpacity>
-
-   {
-    lat == 0 && lon == 0 ?
+    lat == 0 && lon == 0 ? 
+   
     <MapView style={{flex: 1}}
           showsUserLocation={true}
           showsCompass={true}
           zoomControlEnabled={true}
           showsTraffic={true}
           loadingEnabled={true}
-    />   
+    >           
+    </MapView>  
   :
     <MapView style={{flex: 1}}
             region={{
@@ -97,9 +84,39 @@ export default function App() {
             zoomControlEnabled={true}
             showsTraffic={true}
             loadingEnabled={true}
-      />   
-  }
-
+      > 
+      <MapView.Circle
+        center={{
+          latitude: lat,
+          longitude: lon,
+        }}
+        opacity={0.5}
+        radius={700}
+        strokeWidth={2}
+        strokeColor = {idleColor}
+        fillColor={idleColor}
+      />  
+      
+    </MapView> 
+  }  
+    {
+    hidden == true
+    ?
+    <TouchableOpacity onPress={()=>setHidden(false)}>
+        <Image style={{width: 130, height: 130}} source={require('./assets/search.png')} />
+    </TouchableOpacity>
+     :
+    <TextInput
+    style={{ marginTop: 20, width: 120, height: 30, right: -20}}
+    onChangeText={text => onChangeText(text)}
+    placeholder="Springfield,VA"
+    placeholderTextColor="gray"
+    value={value}
+   />
+    }
+     <TouchableOpacity onPress={()=>this.handleFetch()}>
+        <Image style={{width: 130, height: 130}} source={require('./assets/SearchLogo.png')} />
+    </TouchableOpacity>
     </View>
   );
 }
