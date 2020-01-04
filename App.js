4d6a54307dbd1f422663c3842ad910fe7a5314b7
marior9 <mariorodriguezan@gmail.com>
2020-01-04 16:35:59 -0500

    location and raduous working with different colors

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import MapView , { AnimatedRegion }  from'react-native-maps';
-
+import MapView from'react-native-maps';
+import PulseLoader from 'react-native-pulse-loader';
 import {
   Image, 
   TouchableOpacity, 
@@ -14,17 +14,16 @@ from 'react-native';
 export default function App() {
   //using Hooks To set input text 
   const [value, onChangeText] = React.useState('');
-  const [hidden, setHidden] = React.useState(true);
+  const [hidden, setHidden] = React.useState(false);
   const [lat, setLat] = React.useState(0)
   const [lon, setLon] = React.useState(0)
-  const [busyColor, setBusyColor] = React.useState('#fc000046')
- 
+  const [busyColor, setBusyColor] = React.useState('#ffffff3a')
+  const [searchAnimation, setAnimation] = React.useState(false)
+
   const localHost = ""
-  
-  
+
   handleFetch=()=>{
-    console.log("fetch sent")
-    console.log("this is the value: " + value)
+    setAnimation(false) //fix async issu with animation
     const data = { location: value}
     const options = {
       method: 'POST',
@@ -32,13 +31,20 @@ export default function App() {
         'Content-Type': 'application/json',
        },
       body: JSON.stringify(data),
-     
     }
-  
     fetch(`http://${localHost}:3000/api`, options)
     .then(resp=>resp.json())
-    .then(data=>{
-      console.log(data)
+    .then(data=>{()=>{ setAnimation(false) }
+     
+      // Alert.alert(
+      //   'TEST',
+      //   'Information Collected',
+      //   [
+      //     {text: 'OK', onPress: () =>console.log("ok pressed") },
+      //   ]
+      // );
+      
+      // console.log(data)
       setLat(parseFloat(data.lat))
       setLon(parseFloat(data.lon))
 
@@ -48,85 +54,125 @@ export default function App() {
         })
       let total = removeNaN.reduce((a, b) => a + b) / removeNaN.length
       
-      console.log("array of numbers " + numbers)
-      console.log("array of numbers to removed " + removeNaN)
-      console.log("total % " + total)
+      // console.log("array of numbers " + numbers)
+      // console.log("array of numbers to removed " + removeNaN)
+      // console.log("total % " + total)
 
       // Radious color key.
       if(total > 10 && total <35){
         setBusyColor('#02bd0c2a')
       }else if(total > 36 && total < 65){
-        setBusyColor('#e7d42d4f')
+        setBusyColor('#e7d42d4f')       
       }else{
         setBusyColor('#fc000046')
       }
       console.log("received.")
-
+      
     }).catch(err => {
       // Error handling
       console.log("Error Reading data " + err);
     });
   }
-  
-  return (
  
+  return (
+
     <View style={{flex: 1}}>
-    {
-    lat == 0 && lon == 0 ? 
-   
-    <MapView style={{flex: 1}}
-          showsUserLocation={true}
-          showsCompass={true}
-          zoomControlEnabled={true}
-          showsTraffic={true}
-          loadingEnabled={true}
-    >           
-    </MapView>  
-  :
-    <MapView style={{flex: 1}}
-            region={{
-              latitude: lat,
-              longitude: lon,
-              latitudeDelta: 0.0143,
-              longitudeDelta: 0.0134
-            }}
+      {
+      lat == 0 && lon == 0 ? 
+  
+      <MapView style={{flex: 1}}
             showsCompass={true}
             zoomControlEnabled={true}
             showsTraffic={true}
-            loadingEnabled={true}
-      > 
-      <MapView.Circle
-        center={{
-          latitude: lat,
-          longitude: lon,
-        }}
-        opacity={0.5}
-        radius={700}
-        strokeWidth={2}
-        strokeColor = {busyColor}
-        fillColor={busyColor}
-      />  
+            loadingEnabled={true} 
+      >         
+           {
+            searchAnimation == true
+            ?
+          <View>
+              <PulseLoader
+              borderColor='#02ff23a8'
+              backgroundColor='#c915ba5e'
+              pulseMaxSize={700}
+              size={0}
+              avatar={'./assets/SearchLogo.png'}
+              />
+              
+          </View>
+            :
+            null
+          }     
+          <View>
+            {
+            hidden == false
+            ?
+            <TouchableOpacity onPress={()=>setHidden(true)}>
+                <Image style={{
+                  position: 'absolute', 
+                  left: 0, 
+                  top: 0,
+                  width: 130, 
+                  height: 130}} 
+                  source={require('./assets/search.png')} />
+            </TouchableOpacity>
+            :
+            <TextInput
+            style={{ 
+              marginTop: 30,
+              width: 120, 
+              height: 30, 
+              right: -200}}
+            onChangeText={text => onChangeText(text)}
+            placeholder="Springfield,VA"
+            placeholderTextColor="gray"
+            value={value}
+            />
+            }
+            <TouchableOpacity onPress={()=>this.handleFetch()}>
+                <Image style={
+                  {position: 'absolute', 
+                  left: 0, 
+                  top: 80,
+                  width: 130, 
+                  height: 130}} 
+                  source={require('./assets/SearchLogo.png')} />
+            </TouchableOpacity>
+          </View>   
+         
+      </MapView>  
+          :
       
-    </MapView> 
-  }  
-    {
-    hidden == true
-    ?
-    <TouchableOpacity onPress={()=>setHidden(false)}>
-        <Image style={{width: 130, height: 130}} source={require('./assets/search.png')} />
-    </TouchableOpacity>
-     :
-    <TextInput
-    style={{ marginTop: 20, width: 120, height: 30, right: -20}}
-    onChangeText={text => onChangeText(text)}
-    placeholder="Springfield,VA"
-    placeholderTextColor="gray"
-    value={value}
-   />
-    }
-     <TouchableOpacity onPress={()=>this.handleFetch()}>
-        <Image style={{width: 130, height: 130}} source={require('./assets/SearchLogo.png')} />
-    </TouchableOpacity>
+      <MapView 
+          style={{flex: 1}}
+              region={{
+                latitude: lat,
+                longitude: lon,
+                latitudeDelta: 0.0143,
+                longitudeDelta: 0.0134
+              }}
+              showsCompass={true}
+              zoomControlEnabled={true}
+              showsTraffic={true}
+              loadingEnabled={true}
+        > 
+      <MapView.Circle
+          center={{
+            latitude: lat,
+            longitude: lon,
+          }}
+          radius={600}
+          strokeWidth={3}
+          strokeColor = {busyColor}
+          fillColor={busyColor}
+        />  
+        
+      </MapView> 
+      
+        }
+      
+  
     </View>
   );
 }
+
+
