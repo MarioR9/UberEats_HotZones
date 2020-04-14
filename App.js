000110d2473c2267553e7bfc3b1cd0f68297b9ab
marior9 <mariorodriguezan@gmail.com>
2020-04-14 15:27:20 -0400

    alert msg Added

--- App.js
+++ App.js
@@ -1,30 +1,32 @@
 import React from 'react';
-import { Container, Header, Item, Input, Icon, Button, Text, Spinner } from 'native-base';
+import { Container, Header, Item, Input, Icon, Button, Text } from 'native-base';
 import MapView from'react-native-maps';
-
-
 import {
   SafeAreaView,
-  Modal,
-  View,
-  StyleSheet
+  Alert
        }
 from 'react-native';
+
 
 
 export default function App() {
   //using Hooks To set input text 
   const [value, onChangeText] = React.useState('');
-  const [hidden, setHidden] = React.useState(false);
   const [lat, setLat] = React.useState(0)
   const [lon, setLon] = React.useState(0)
   const [busyColor, setBusyColor] = React.useState('#ffffff3a')
-  const [modalVisible, setModalVisible] = React.useState(false);
   
   const localHost = "IPADRESS"
 
   handleFetch=()=>{
-    setModalVisible(true)
+
+    Alert.alert(
+      'Collecting Information',
+      'Please Wait..',
+      [
+        {text: 'OK', onPress: () =>console.log("ok pressed") },
+      ]
+    );
     const data = { location: value}
     const options = {
       method: 'POST',
@@ -36,17 +38,16 @@ export default function App() {
     fetch(`http://${localHost}:3000/api`, options)
     .then(resp=>resp.json())
     .then(data=>{()=>
-     
-      // Alert.alert(
-      //   'TEST',
-      //   'Information Collected',
-      //   [
-      //     {text: 'OK', onPress: () =>console.log("ok pressed") },
-      //   ]
-      // );
+
+      Alert.alert(
+        'Success',
+        'Thank you for waiting',
+        [
+          {text: 'OK', onPress: () =>console.log("ok pressed") },
+        ]
+      );
       
       // console.log(data)
-      setModalVisible(false)
       setLat(parseFloat(data.lat))
       setLon(parseFloat(data.lon))
 
@@ -80,16 +81,6 @@ export default function App() {
 
     <SafeAreaView style={{flex: 1}}>
        <Container>
-       <Modal
-          animationType="slide"
-          transparent={true}
-          visible={modalVisible}>
-
-          <View style={styles.centeredView}>
-          <Spinner style={styles.modalText}/>
-        </View>
-        </Modal>
-        
         <Header searchBar rounded>
           <Item>
             <Icon name="ios-search" />
@@ -123,59 +114,20 @@ export default function App() {
           zoomControlEnabled={true}
           showsTraffic={true}
           loadingEnabled={true}
-    > 
-  <MapView.Circle
-      center={{
-        latitude: lat,
-        longitude: lon,
-      }}
-      radius={600}
-      strokeWidth={3}
-      strokeColor = {busyColor}
-      fillColor={busyColor}
-    />  
-  </MapView> 
-  }
+          > 
+      <MapView.Circle
+          center={{
+            latitude: lat,
+            longitude: lon,
+          }}
+          radius={600}
+          strokeWidth={3}
+          strokeColor = {busyColor}
+          fillColor={busyColor}
+        />  
+      </MapView> 
+      }
       </Container>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5
-  },
-  openButton: {
-    backgroundColor: "#F194FF",
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center"
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center"
-  }
-});
